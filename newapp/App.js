3ef60f5856f7a4385a4474a85090ab70474d1b17
Somya Ranjan Singh <maaheersj@gmail.com>
2023-08-06 12:00:06 +0530

    added native basic

--- newapp/App.js
+++ newapp/App.js
@@ -6,13 +6,15 @@ import {
 } from 'react-native';
 
 function App(){
-
+ const pressBtn = () => {
+  console.warn("pressed")
+ }
   return (
     <View>
       <Text style={{fontSize: 25}}>Hello React-Native</Text>
       <Text style={{fontSize: 25}}>Learning React-Native</Text>
       <Text style={{fontSize: 25}}>Learning to make Hybrid application</Text>
-      <Button title='Tap me'></Button>
+      <Button title='Press Here' color={"green"} onPress={pressBtn}></Button>
     </View>
   );
 }
