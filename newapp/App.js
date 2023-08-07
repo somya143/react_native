0a1b64b3ce994bebdb6834c00f227cb0526ae043
Somya Ranjan Singh <maaheersj@gmail.com>
2023-08-07 18:37:53 +0530

    done with input text

--- newapp/App.js
+++ newapp/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  TextInput
 } from 'react-native';
 import ExStyle from "./styles";
 function App(){
  const pressBtn = () => {
   console.warn("pressed")
- }
+ };
+ const [name,setName] = useState("");
+
   return (
     <View>
       <Text style={styles.textStyles}>Hello React-Native</Text>
-      <Text style={styles.textStyles}>Learning React-Native</Text>
-      <Text style={styles.textStyles}>Making Hybrid application</Text>
-      <Text style={[styles.textStyles,ExStyle,{backgroundColor: "gray"}]}>Making Hybrid application</Text>
-      <Button title='Press Here' color={"green"} onPress={pressBtn}></Button>
+      <Text style={{fontSize:20,color:"black",marginTop:20,paddingLeft:10}}>Your Name is : {name}</Text>
+      <TextInput placeholder='Enter Name' style={styles.textInput} 
+      value={name}
+      onChangeText={(e) => setName(e)}
+      />
+      <Button title='Clear Text' onPress={() => setName("")} />
     </View>
+
   );
 }
 
@@ -29,8 +35,16 @@ const styles = StyleSheet.create({
    padding : 30,
    textAlign : "center",
    marginVertical : 10
-  } 
-
+  },
+  textInput : {
+  color: "blue",
+  fontSize: 20,
+  margin: 10,
+  borderColor: "blue",
+  borderWidth: 2,
+  paddingLeft : 10
+  }  
+  
 })
 
 
